f98b106e42d9b1a90b4f250dd1b687dcfcb98834
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 21:41:34 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ item_obj[item] = Math.floor(Math.random() * 100);
+ cart.push(item_obj);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
