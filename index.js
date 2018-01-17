ce2e37deb37bc5e63385daec8f9a4239c03db302
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 23:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length === 0) {
-    return "Your shopping cart is empty.";
+    console.log("Your shopping cart is empty.");
   } else {
     var message = `In your cart, you have `;
     for (var i = 0; i < cart.length; i++) {
