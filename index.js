ac07b9a80d3bf0f9794260f40c50c47c87576433
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 23:53:19 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- item_obj[item] = Math.floor(Math.random() * 100);
+ var item_obj = {[item]: Math.floor(Math.random() * 100)};
  cart.push(item_obj);
  console.log(`${item} has been added to your cart.`);
  return cart;
@@ -19,16 +19,74 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var message, item_hash, item, price;
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    message = `In your cart, you have `;
+
+    if (cart.length === 1) {
+      item_hash = cart[0];
+      item = Object.keys(item_hash)[0];
+      price = item_hash[item];
+      message += `${item} at $${price}.`;
+    } else if(cart.length === 2) {
+      item_hash = cart[0];
+      item = Object.keys(item_hash)[0];
+      price = item_hash[item];
+      message += `${item} at $${price}`;
+      item_hash = cart[1];
+      item = Object.keys(item_hash)[0];
+      price = item_hash[item];
+      message += ` and ${item} at $${price}.`;
+    } else {
+      item_hash = cart[0];
+      item = Object.keys(item_hash)[0];
+      price = item_hash[item];
+      message += `${item} at $${price}`;
+      for (var i = 1; i < cart.length - 1; i++) {
+        item_hash = cart[i];
+        item = Object.keys(item_hash)[0];
+        price = item_hash[item];
+        message += `, ${item} at $${price}`;
+      }
+      item_hash = cart[cart.length - 1];
+      item = Object.keys(item_hash)[0];
+      price = item_hash[item];
+      message += `, and ${item} at $${price}.`;
+    }
+    console.log(message);
+  }
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])[0];
+    total += cart[i][item];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    var hash_item = Object.keys(cart[i])[0];
+    if (hash_item === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart = [];
 }
